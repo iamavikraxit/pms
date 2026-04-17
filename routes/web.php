47a248991e262f0f08c2d5039a410c43d3a0bc72
Avik Raxit <avik.prof@gmail.com>
2026-04-17 12:36:04 +0530

    fix: Add missing newline at end of file in web routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.home');
-})->name('home');   
+})->name('home');
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+})->name('contact');
